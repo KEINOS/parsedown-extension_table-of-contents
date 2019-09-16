e41ab0236d3acf3b1b0c6df35420615d235ffc6e
KEINOS <github@keinos.com>
2019-09-17 00:13:26 +0900

    fix: Prevent getting "null" value in URL to download

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -11,6 +11,7 @@ cd $(cd $(dirname $0); pwd)
 # -----------------------------------------------------------------------------
 YES=0
 NO=1
+URL_API_GITHUB='https://api.github.com/repos/erusev/parsedown/releases/latest'
 
 # -----------------------------------------------------------------------------
 #  Check Basic requirements
@@ -62,15 +63,27 @@ curl --version
 #  Download Latest Parsedown from releases page
 # -----------------------------------------------------------------------------
 [ -f './Parsedown.php' ] || {
-    # Get URL of latest tarball
+    echo -n '- Searching URL of Parsedown archive .'
+    # Loop here until getting the tarball URL, since Travis CI requests many
+    # times at once, the GitHub API sometimes returns 503 status error.
     while :
     do
+        echo -n '.'
         sleep $[ ( $RANDOM % 10 )  + 1 ]s
-        url_download_tarboll=$(curl -s https://api.github.com/repos/erusev/parsedown/releases/latest | jq -r '.tarball_url')
-        [ -z "$url_download_tarboll" ] && continue || break
-    done
 
-    echo '- URL of tarball:' $url_download_tarboll
+        curl --silent --head $URL_API_GITHUB | grep Status | grep 200 > /dev/null 2>&1 && \
+        url_download_tarboll=$(curl -s $URL_API_GITHUB | jq -r '.tarball_url')
+
+        if [ -z "${url_download_tarboll}" ]; then
+            continue
+        elif [ "${url_download_tarboll}" = "null" ]; then
+            continue
+        else
+            echo '. OK'
+            echo '- URL of tarball found:' $url_download_tarboll
+            break;
+        fi
+    done
 
     # Get Name of the archive
     #name_file_archive=$(basename "$url_download_tarboll")
