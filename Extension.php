7c76b4f865a1e6bdbfff164f8f0add225c7b0448
KEINOS <github@keinos.com>
2019-09-22 00:32:10 +0900

    feat: [toc] tag (Issue #2)

--- Extension.php
+++ Extension.php
@@ -4,31 +4,37 @@
  * ============================================================================
  * It creates a list of contents table from headings.
  *
- * @author  KEINOS (https://github.com/KEINOS/)
- * @package Parsedown ^1.7 (https://github.com/erusev/parsedown)
- * @see Howto: https://github.com/KEINOS/parsedown-extension_table-of-contents/
- * @license https://github.com/KEINOS/parsedown-extension_table-of-contents/LICENSE
+ * @author    KEINOS (https://github.com/KEINOS/)
+ * @package   Parsedown ^1.7 (https://github.com/erusev/parsedown)
+ * @php       ^5.6.40
+ * @see       Howto: https://github.com/KEINOS/parsedown-extension_table-of-contents/
+ * @license   https://github.com/KEINOS/parsedown-extension_table-of-contents/LICENSE
 */
 
-class ParsedownToc extends \Parsedown
-{
-    /* Methods/Functions (in ABC order) */
+// Old version compatibility
+class Extension extends ParsedownToC{}
 
-    #
-    # Block
-    #
-    protected function blockHeader($Line)
+class ParsedownToC extends \Parsedown
+{
+    const TAG_TOC = '[toc]';
+
+    /* ========================================================================
+      Methods/Functions (in ABC order)
+    ======================================================================== */
+
+    // Overriding parent method: \Parsedown::blockHeader()
+    protected function blockHeader(array $Line)
     {
         if (isset($Line['text'][1])) {
             $Block = \Parsedown::blockHeader($Line);
 
             // Compatibility with old Parsedown Version
             if (isset($Block['element']['handler']['argument'])) {
-                $text  = $Block['element']['handler']['argument'];
+                $text = $Block['element']['handler']['argument'];
             }
 
             if (isset($Block['element']['text'])) {
-                $text  = $Block['element']['text'];
+                $text = $Block['element']['text'];
             }
 
             $level = $Block['element']['name'];    //levels are h1, h2, ..., h6
@@ -50,49 +56,64 @@ class ParsedownToc extends \Parsedown
         }
     }
 
-    public function contentsList($Return_as = 'string')
+    // Alias of parent method: \Parsedown::text()
+    public function body($text)
     {
-        if ('string' === strtolower($Return_as)) {
+        return \Parsedown::text($text);
+    }
+
+    public function contentsList($type_return = 'string')
+    {
+        if ('string' === strtolower($type_return)) {
             $result = '';
             if (! empty($this->contentsListString)) {
-                $result = $this->text($this->contentsListString);
+                $result = $this->body($this->contentsListString);
             }
             return $result;
         }
-        if ('json' === strtolower($Return_as)) {
+        if ('json' === strtolower($type_return)) {
             return json_encode($this->contentsListArray);
         }
 
-        error_log('Unknown return type given while parsing ToC. At: ' . __FUNCTION__ . '() in Line:' . __LINE__ . ' (Using default type)');
+        error_log(
+            'Unknown return type given while parsing ToC.'
+            . ' At: ' . __FUNCTION__ . '() '
+            . ' in Line:' . __LINE__ . ' (Using default type)'
+        );
         return $this->contentsList('string');
     }
 
-    protected function createAnchorID($Text)
+    protected function createAnchorID($text)
     {
-        return  urlencode($this->fetchText($Text));
+        return  urlencode($this->fetchText($text));
     }
 
-    protected function fetchText($Text)
+    protected function fetchText($text)
     {
-        return trim(strip_tags($this->line($Text)));
+        return trim(strip_tags($this->line($text)));
     }
 
-    #
-    # Setters
-    #
-    protected function setContentsList($Content)
+    protected function replaceTagToC($html){
+        $toc     = $this->contentsList();
+        $needle  = '<p>' . self::TAG_TOC . '</p>';
+        $replace = '<div id="toc">' . $toc . '</div>';
+
+        return str_replace($needle, $replace, $html);
+    }
+
+    protected function setContentsList(array $Content)
     {
         $this->setContentsListAsArray($Content);
         $this->setContentsListAsString($Content);
     }
 
-    protected function setContentsListAsArray($Content)
+    protected function setContentsListAsArray(array $Content)
     {
         $this->contentsListArray[] = $Content;
     }
     protected $contentsListArray = array();
 
-    protected function setContentsListAsString($Content)
+    protected function setContentsListAsString(array $Content)
     {
         $text  = $this->fetchText($Content['text']);
         $id    = $Content['id'];
@@ -115,5 +136,17 @@ class ParsedownToc extends \Parsedown
     }
     protected $contentsListString = '';
     protected $firstHeadLevel = 0;
+
+    // Overriding parent method: \Parsedown::text()
+    public function text($text)
+    {
+        $body = $this->body($text);
+
+        if( strpos($text, self::TAG_TOC) === false ){
+            return $body;
+        }
+
+        return $this->replaceTagToC($body);
+    }
 
 }
