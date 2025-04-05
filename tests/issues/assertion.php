14e636790790cccf0e4a66deb3053e25fc1573a1
KEINOS <github@keinos.com>
2025-04-05 14:21:39 +0900

    chore: add assertNotEqual function for testing

--- tests/issues/assertion.php
+++ tests/issues/assertion.php
@@ -12,7 +12,31 @@
 function assertEqual($description, $actual, $expect)
 {
     if ($actual !== $expect) {
-        echo $description . " ... NG (unexpected result)\n";
+        echo $description . " ... NG (inputs should be equal)\n";
+        echo "  - Expected:\n" . preg_replace('/^/m', '    ', $expect) . "\n";
+        echo "  - Actual:\n"   . preg_replace('/^/m', '    ', $actual) . "\n";
+
+        return false;
+    }
+
+    echo $description . " ... OK\n";
+
+    return true;
+}
+
+/**
+ * Returns true if $actual and $expect strings are NOT equal.
+ * It will print out the result of the test as well.
+ *
+ * @param  string $description  Description of the test.
+ * @param  string $actual       Actual result.
+ * @param  string $expect       Expected result.
+ * @return bool                 True if $actual and $expect are not equal.
+ */
+function assertNotEqual($description, $actual, $expect)
+{
+    if ($actual == $expect) {
+        echo $description . " ... NG (input should not be equal)\n";
         echo "  - Expected:\n" . preg_replace('/^/m', '    ', $expect) . "\n";
         echo "  - Actual:\n"   . preg_replace('/^/m', '    ', $actual) . "\n";
 
